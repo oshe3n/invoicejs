87bedf9aae5022742c1a51b0190f35fad8689c39
oshe3n <42730612+oshe3n@users.noreply.github.com>
2019-08-20 12:26:01 +0530

    Add files via upload

--- bot.js
+++ bot.js
@@ -9,12 +9,16 @@ var SPFetchClient = require("@pnp/nodejs").SPFetchClient;
 var Invoices = require('./object').Invoices;
 const poCard = require('./Card1.json');
 const invCard = require('./Card2.json');
-const invDetail = require('./Card3.json');
+var invDetail = require('./Card3.json');
+var newcard = require('./Card4.json');
+
+
+var z = '{"type":"ColumnSet","columns":[{"type":"Column","items":[{"type":"TextBlock","text":"date1","wrap":true}],"width":"auto"},{"type":"Column","spacing":"Medium","items":[{"type":"TextBlock","text":"number1","wrap":true}],"width":"stretch"},{"type":"Column","items":[{"type":"TextBlock","text":"am1","wrap":true}],"width":"auto"}]}';
 
 var invoices = new Invoices();    
 
 sp.setup({
-    sp: {
+    sp: {   
         fetchClientFactory: () => {
             return new SPFetchClient("https://m365x628217.sharepoint.com/sites/TestTeamsMIP", "c4ab9843-65f4-41e9-8a49-c5e04881f0db", "LtjTLjFxgYFZnG6D1YFfGlMzMZzvwI/BuU4DODu1v+I=");
         },
@@ -39,23 +43,40 @@ class MyBot extends ActivityHandler {
                 
                 var txt = context.activity.text;
                 
-                var aatach=[]
-                
+                var json= {};                
+                json = Object.assign({},newcard);  
+                json.body=[];                
+                json.body.push(JSON.parse('{"type":"Container","style":"emphasis","items":[{"type":"ColumnSet","columns":[{"type":"Column","items":[{"type":"TextBlock","size":"Large","weight":"Bolder","text":"**INVOICE SUMMARY**"}],"width":"stretch"}]}],"bleed":true}'));
+                json.body.push(JSON.parse('{"type":"Container","items":[{"type":"ColumnSet","columns":[{"type":"Column","items":[{"type":"TextBlock","size":"Large","text":"PO Number : '+txt+'","wrap":true}],"width":"stretch"},{"type":"Column","items":[{"type":"ActionSet","actions":[{"type":"Action.OpenUrl","title":"EXPORT AS PDF","url":"https://m365x628217.sharepoint.com/sites/TestTeamsMIP"}]}],"width":"auto"}]}]}'))
+                json.body.push(JSON.parse('{"type":"Container","spacing":"Large","style":"emphasis","items":[{"type":"ColumnSet","columns":[{"type":"Column","items":[{"type":"TextBlock","weight":"Bolder","text":"DATE"}],"width":"auto"},{"type":"Column","spacing":"Large","items":[{"type":"TextBlock","weight":"Bolder","text":"INVOICE NUMBER"}],"width":"stretch"},{"type":"Column","items":[{"type":"TextBlock","weight":"Bolder","text":"AMOUNT"}],"width":"auto"}]}],"bleed":true}'))
+
+                var tot = 0;
+
                 await sp.web.lists.getByTitle("INV_LIST").items.get().then((items) => {
-                    items.forEach(element => {
+                    items.forEach(element => {                                              
                         if(element.Title==txt)
-                        {                        
-                            invDetail.body[1].facts[0].value = txt
-                            invDetail.body[1].facts[1].value = element.InvoiceNumber
-                            invDetail.body[1].facts[2].value = element.InvoiceDate
-                            invDetail.body[1].facts[3].value = element.InvoiceAmmount                            
-                            aatach.push(CardFactory.adaptiveCard(invDetail))                                                                
+                        {         
+                            var k = JSON.parse(z);                                                                                                           
+                            k.columns[0].items[0].text = element.InvoiceDate
+                            k.columns[1].items[0].text = element.InvoiceNumber
+                            k.columns[2].items[0].text = element.InvoiceAmmount
+                            json.body.push(k);                                 
+                            tot+= parseInt(element.InvoiceAmmount);
+                            /*var json= new Object();
+                            json = Object.assign({},invDetail);                                                                          
+                            json.body[1].facts[0].value = txt
+                            json.body[1].facts[1].value = element.InvoiceNumber                            
+                            json.body[1].facts[2].value = element.InvoiceDate
+                            json.body[1].facts[3].value = element.InvoiceAmmount                            
+                            aatach.push(CardFactory.adaptiveCard(json)) */
                         }        
                     });    
-                });                
+                });                            
+                
+                json.body.push(JSON.parse('{"type":"Container","style":"emphasis","items":[{"type":"ColumnSet","columns":[{"type":"Column","items":[{"type":"TextBlock","horizontalAlignment":"Right","text":"Total","wrap":true}],"width":"stretch"},{"type":"Column","items":[{"type":"TextBlock","weight":"Bolder","text":"'+tot+'"}],"width":"auto"}]}],"bleed":true}'));
 
                 await context.sendActivity({
-                    attachments: aatach
+                    attachments: [CardFactory.adaptiveCard(json)]
                 });    
 
                 flag=false;
